162c72ff508ff7d266eabc3cb8a465d49d322ee6
Rafael Campos <rafa.campos.200.rc@gmail.com>
2022-04-01 11:07:13 -0300

    ajusta responsabilidades

--- escavador/resources/endpoint.py
+++ escavador/resources/endpoint.py
@@ -5,5 +5,3 @@ class Endpoint(object):
 
     def __init__(self):
         self.methods = Method()
-        self.states = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF',' ES', 'GO', 'MA', 'MS', 'MT', 'MG', 'PA', 'PB', 'PR',
-                       'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
---
+++ escavador/resources/utils.py
@@ -0,0 +1,7 @@
+class Utils(object):
+
+    @staticmethod
+    def valid_states():
+        valid_states = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', ' ES', 'GO', 'MA', 'MS', 'MT', 'MG', 'PA', 'PB', 'PR',
+                        'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+        return valid_states
