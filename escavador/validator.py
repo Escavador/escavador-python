b9c70c45e84f5b5f5eaff3bcbfeefaabc65beb9d
Rafael Campos <rafa.campos.200.rc@gmail.com>
2022-04-04 11:43:34 -0300

    ajusta validação

--- escavador/validator.py
+++ escavador/validator.py
@@ -2,6 +2,5 @@ class Validator(object):
 
     @staticmethod
     def valid_states():
-        valid_states = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', ' ES', 'GO', 'MA', 'MS', 'MT', 'MG', 'PA', 'PB', 'PR',
-                        'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
-        return valid_states
+        return ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', ' ES', 'GO', 'MA', 'MS', 'MT', 'MG', 'PA', 'PB', 'PR','PE',
+                'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
