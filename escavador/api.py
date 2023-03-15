8a0b12c9100730fbbf6255e60d093bc0a5b99374
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2023-03-15 13:55:12 -0300

    Adapta Api para fazer requests com query params

--- escavador/api.py
+++ escavador/api.py
@@ -1,4 +1,6 @@
 import os
+from typing import Dict, Union
+
 import requests
 
 import escavador
@@ -27,20 +29,39 @@ class Api(object):
             except KeyError:
                 raise ApiKeyNotFoundException("Nenhuma chave da API foi informada")
 
-    def headers(self):
+    def headers(self) -> Dict:
+        """
+        Retorna os headers padrões para a API
+
+        :return: Dict de headers
+        """
         return {
             'User-Agent': 'escavador-python/' + version('escavador'),
             'Authorization': 'Bearer ' + self.api_key,
             'X-Requested-With': 'XMLHttpRequest'
         }
 
-    def request(self, method, url, **kwargs):
+    def request(self, method: str,
+                url: str,
+                data: Dict = None,
+                params: Dict = None,
+                **kwargs) -> Union[Dict, bytes]:
+        """
+        Executa um request HTML para a API
+
+        :param method: método HTML
+        :param url: slug do endpoint a ser chamado
+        :param data: dados a serem enviados no formato json
+        :param params: parâmetros a serem enviados na URL
+        :return: Union[Dict, bytes]
+        """
         url = parse.urljoin(self.base_url, url)
-        data = kwargs.get('data')
         if data is not None:
-            data = {key: value for key, value in kwargs.get('data').items() if value is not None}
+            data = {k: v for k, v in data.items() if v is not None}
+        if params is not None:
+            params = {k: v for k, v in params.items() if v is not None}
         with requests.Session() as session:
-            with session.request(method=method, url=url, headers=self.headers(), json=data) as resp:
+            with session.request(method=method, url=url, headers=self.headers(), json=data, params=params) as resp:
                 if resp.headers['Content-Type'] == 'application/pdf':
                     return resp.content
                 else:
--- escavador/method.py
+++ escavador/method.py
@@ -6,14 +6,14 @@ class Method(object):
     def __init__(self, api_version):
         self.api = Api(version=api_version)
 
-    def get(self, url, **kwargs):
-        return self.api.request('GET', url, data=kwargs.get('data'))
+    def get(self, url, *, data=None, params=None, **kwargs):
+        return self.api.request('GET', url, data=data, params=params, **kwargs)
 
-    def post(self, url, **kwargs):
-        return self.api.request('POST', url, data=kwargs.get('data'))
+    def post(self, url, *, data=None, params=None, **kwargs):
+        return self.api.request('POST', url, data=data, params=params, **kwargs)
 
-    def put(self, url, **kwargs):
-        return self.api.request('PUT', url, data=kwargs.get('data'))
+    def put(self, url, *, data=None, params=None, **kwargs):
+        return self.api.request('PUT', url, data=data, params=params, **kwargs)
 
-    def delete(self, url, **kwargs):
-        return self.api.request('DELETE', url, data=kwargs.get('data'))
+    def delete(self, url, *, data=None, params=None, **kwargs):
+        return self.api.request('DELETE', url, data=data, params=params, **kwargs)
