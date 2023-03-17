3edfee074a5d79a36d430eebc20ffebd63b0b1ff
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2023-03-17 15:36:47 -0300

    Implementa busca especificando quantidade na V2

--- escavador/v2/src/buscaprocesso.py
+++ escavador/v2/src/buscaprocesso.py
@@ -1,3 +1,4 @@
+import re
 from enum import Enum
 
 from escavador.resources.helpers.endpoint import Endpoint
@@ -6,7 +7,7 @@ from typing import Optional, Dict, List, Union
 
 
 class BuscaProcesso(Endpoint):
-    class Ordenacao(Enum):
+    class CriterioOrdenacao(Enum):
         ULTIMA_MOVIMENTACAO = "data_ultima_movimentacao"
         INICIO = "data_inicio"
 
@@ -25,13 +26,16 @@ class BuscaProcesso(Endpoint):
 
         return self.methods.get(f"processos/numero_cnj/{numero_cnj}", data=data)
 
-    def movimentacoes(self, numero_cnj: str, **kwargs) -> Dict:
+    def movimentacoes(self, numero_cnj: str, qtd: int = 100, **kwargs) -> Dict:
         """
         Retorna as movimentações de um processo pelo seu número único do CNJ.
         :param numero_cnj: o número único do CNJ do processo
+        :param qtd: quantidade desejada de movimentações a ser retornada pela query
         :return:
 
         >>> BuscaProcesso().movimentacoes("0000000-00.0000.0.00.0000") # doctest: +SKIP
+
+        >>> BuscaProcesso().movimentacoes("0000000-00.0000.0.00.0000", qtd=10) # doctest: +SKIP
         """
         data = kwargs
 
@@ -39,9 +43,10 @@ class BuscaProcesso(Endpoint):
 
     def por_nome(self,
                  nome: str,
-                 ordena_por: Optional[Ordenacao] = None,
+                 ordena_por: Optional[CriterioOrdenacao] = None,
                  ordem: Optional[Ordem] = None,
                  tribunais: Optional[List[str]] = None,
+                 qtd: int = 100,
                  **kwargs) -> Dict:
         """
         Retorna os processos envolvendo uma pessoa ou empresa a partir do seu nome.
@@ -49,21 +54,29 @@ class BuscaProcesso(Endpoint):
         :param ordena_por: critério de ordenação
         :param ordem: determina ordenação ascendente ou descendente
         :param tribunais: lista de siglas de tribunais para filtrar a busca
+        :param qtd: quantidade desejada de processos a ser retornada pela query
 
         >>> BuscaProcesso().por_nome("Escavador Engenharia e Construcoes Ltda",
-        ...                          ordena_por=BuscaProcesso.Ordenacao.INICIO,
+        ...                          ordena_por=BuscaProcesso.CriterioOrdenacao.INICIO,
         ...                          ordem=Ordem.DESC,
-        ...                          tribunais=['TJBA', 'TRF1']) # doctest: +SKIP
+        ...                          tribunais=['TJBA', 'TRF1'],
+        ...                          qtd=1) # doctest: +SKIP
 
         >>> BuscaProcesso().por_nome("Escavador Engenharia e Construcoes Ltda") # doctest: +SKIP
         """
-        return self.por_envolvido(nome=nome, ordena_por=ordena_por, ordem=ordem, tribunais=tribunais, **kwargs)
+        return self.por_envolvido(nome=nome,
+                                  ordena_por=ordena_por,
+                                  ordem=ordem,
+                                  tribunais=tribunais,
+                                  qtd=qtd,
+                                  **kwargs)
 
     def por_cpf(self,
                 cpf: str,
-                ordena_por: Optional[Ordenacao] = None,
+                ordena_por: Optional[CriterioOrdenacao] = None,
                 ordem: Optional[Ordem] = None,
                 tribunais: Optional[List[str]] = None,
+                qtd: int = 100,
                 **kwargs) -> Dict:
         """
         Retorna os processos envolvendo uma pessoa a partir de seu CPF.
@@ -71,21 +84,29 @@ class BuscaProcesso(Endpoint):
         :param ordena_por: critério de ordenação
         :param ordem: determina ordenação ascendente ou descendente
         :param tribunais: lista de siglas de tribunais para filtrar a busca
+        :param qtd: quantidade desejada de processos a ser retornada pela query
 
         >>> BuscaProcesso().por_cpf("12345678999",
-        ...                         ordena_por=BuscaProcesso.Ordenacao.ULTIMA_MOVIMENTACAO,
+        ...                         ordena_por=BuscaProcesso.CriterioOrdenacao.ULTIMA_MOVIMENTACAO,
         ...                         ordem=Ordem.ASC,
-        ...                         tribunais=['STF']) # doctest: +SKIP
+        ...                         tribunais=['STF'],
+        ...                         qtd=200) # doctest: +SKIP
 
         >>> BuscaProcesso().por_cpf("123.456.789-99") # doctest: +SKIP
         """
-        return self.por_envolvido(cpf_cnpj=cpf, ordena_por=ordena_por, ordem=ordem, tribunais=tribunais, **kwargs)
+        return self.por_envolvido(cpf_cnpj=cpf,
+                                  ordena_por=ordena_por,
+                                  ordem=ordem,
+                                  tribunais=tribunais,
+                                  qtd=qtd,
+                                  **kwargs)
 
     def por_cnpj(self,
                  cnpj: str,
-                 ordena_por: Optional[Ordenacao] = None,
+                 ordena_por: Optional[CriterioOrdenacao] = None,
                  ordem: Optional[Ordem] = None,
                  tribunais: Optional[List[str]] = None,
+                 qtd: int = 100,
                  **kwargs) -> Dict:
         """
         Retorna os processos envolvendo uma instituição a partir de seu CNPJ.
@@ -93,36 +114,51 @@ class BuscaProcesso(Endpoint):
         :param ordena_por: critério de ordenação
         :param ordem: determina ordenação ascendente ou descendente
         :param tribunais: lista de siglas de tribunais para filtrar a busca
+        :param qtd: quantidade desejada de processos a ser retornada pela query
 
         >>> BuscaProcesso().por_cnpj("07.838.351/0021.60",
-        ...                          ordena_por=BuscaProcesso.Ordenacao.ULTIMA_MOVIMENTACAO,
+        ...                          ordena_por=BuscaProcesso.CriterioOrdenacao.ULTIMA_MOVIMENTACAO,
         ...                          ordem=Ordem.ASC,
-        ...                          tribunais=['TJBA', 'TRF1']) # doctest: +SKIP
+        ...                          tribunais=['TJBA', 'TRF1'],
+        ...                          qtd=1) # doctest: +SKIP
 
         >>> BuscaProcesso().por_cnpj("07838351002160") # doctest: +SKIP
         """
-        return self.por_envolvido(cpf_cnpj=cnpj, ordena_por=ordena_por, ordem=ordem, tribunais=tribunais, **kwargs)
+        return self.por_envolvido(cpf_cnpj=cnpj,
+                                  ordena_por=ordena_por,
+                                  ordem=ordem,
+                                  tribunais=tribunais,
+                                  qtd=qtd,
+                                  **kwargs)
 
     def por_envolvido(self,
                       cpf_cnpj: Optional[str] = None,
                       nome: Optional[str] = None,
-                      ordena_por: Optional[Ordenacao] = None,
+                      ordena_por: Optional[CriterioOrdenacao] = None,
                       ordem: Optional[Ordem] = None,
                       tribunais: Optional[List[str]] = None,
+                      qtd: int = 100,
                       **kwargs) -> Dict:
         """
         Retorna os processos envolvendo uma pessoa ou instituição a partir de seu nome e/ou CPF/CNPJ.
+
+        Caso seja necessário múltiplos requests para obter a quantidade de processos desejada e algum
+        erro ocorra em um request intermediário, a função retorna todos os processos obtidos até o
+        momento com o o status code do erro ocorrido nesse último request.
+
         :param nome: o nome da pessoa ou instituição. Obrigatório se não for informado o CPF/CNPJ
         :param cpf_cnpj: o CPF/CNPJ da pessoa ou instituição. Obrigatório se não for informado o nome
         :param ordena_por: critério de ordenação
         :param ordem: determina ordenação ascendente ou descendente
         :param tribunais: lista de siglas de tribunais para filtrar a busca
+        :param qtd: quantidade desejada de processos a ser retornada pela query
 
-        >>> BuscaProcesso().por_envolvido(nome="Potelo Sistemas de Informacao",
-        ...                               ordena_por=BuscaProcesso.Ordenacao.ULTIMA_MOVIMENTACAO,
+
+        >>> BuscaProcesso().por_envolvido(nome='Escavador Engenharia e Construcoes Ltda',
+        ...                               ordena_por=BuscaProcesso.CriterioOrdenacao.ULTIMA_MOVIMENTACAO,
         ...                               ordem=Ordem.ASC,
-        ...                               tribunais=['TRF4']) # doctest: +SKIP
-
+        ...                               tribunais=['TJBA'],
+        ...                               qtd=0)
         >>> BuscaProcesso().por_envolvido(cpf_cnpj="07.838.351/0021.60") # doctest: +SKIP
         """
         data = {
@@ -136,27 +172,52 @@ class BuscaProcesso(Endpoint):
             'ordem': ordem.value if ordem else None,
         }
 
-        return self.methods.get(f"envolvido/processos", data=data, params=params, **kwargs)
+        current_response = self.methods.get(f"envolvido/processos", data=data, params=params, **kwargs)
+        while 0 < len(current_response['resposta'].get('items', [])) < qtd:
+            cursor = current_response['resposta']['links']['next']
+            if not cursor:
+                break
+
+            next_response = self.__get_more(cursor, **kwargs)
+            next_items = next_response['resposta'].get('items')
+            if not next_items:
+                current_response['http_status'] = next_response['http_status']
+                current_response['success'] = next_response['success']
+                break
+
+            current_response['resposta']['items'].extend(next_items)
+
+            next_cursor = next_response['resposta']['links']['next']
+            if not next_cursor:
+                break
+
+            current_response['resposta']['links']['next'] = next_cursor
+
+        return self.__selecionar_alguns(current_response, qtd)
 
     def por_oab(self,
                 numero: Union[str, int],
                 estado: str,
-                ordena_por: Optional[Ordenacao] = None,
+                ordena_por: Optional[CriterioOrdenacao] = None,
                 ordem: Optional[Ordem] = None,
+                qtd: int = 100,
+                **kwargs
                 ) -> Dict:
         """
-        Retorna os processos de um advogado a partir de número da OAB.
+        Retorna os processos de um advogado a partir de sua carteira da OAB.
         :param numero: o número da OAB
         :param estado: o estado de origem da OAB
         :param ordena_por: critério de ordenação
         :param ordem: determina ordenação ascendente ou descendente
+        :param qtd: quantidade desejada de processos a ser retornada pela query
 
         >>> BuscaProcesso().por_oab(1234, "AC") # doctest: +SKIP
 
         >>> BuscaProcesso().por_oab(numero="123456",
         ...                         estado="SP",
-        ...                         ordena_por=BuscaProcesso.Ordenacao.ULTIMA_MOVIMENTACAO,
-        ...                         ordem=Ordem.DESC) # doctest: +SKIP
+        ...                         ordena_por=BuscaProcesso.CriterioOrdenacao.ULTIMA_MOVIMENTACAO,
+        ...                         ordem=Ordem.DESC,
+        ...                         qtd=1) # doctest: +SKIP
         """
         data = {
             'oab_numero': f"{numero}",
@@ -167,3 +228,14 @@ class BuscaProcesso(Endpoint):
             'ordem': ordem.value if ordem else None,
         }
         return self.methods.get(f"advogado/processos", data=data, params=params)
+
+    def __get_more(self, cursor: str, **kwargs) -> Dict:
+        """Consome um cursor para obter os próximos resultados de uma busca"""
+        endpoint_cursor = re.sub(r".*/api/v\d/", "", cursor)
+        return self.methods.get(endpoint_cursor, **kwargs)
+
+    @staticmethod
+    def __selecionar_alguns(resposta: Dict, qtd: int) -> Dict:
+        """Seleciona os itens de uma resposta de acordo com a quantidade desejada"""
+        resposta['resposta']['items'] = resposta['resposta']['items'][:qtd]
+        return resposta
