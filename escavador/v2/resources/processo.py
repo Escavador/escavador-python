e588a160bc24414e56183bfe6537b73b1747c509
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2023-03-24 16:36:40 -0300

    Comentários nas dataclasses

--- escavador/v2/resources/processo.py
+++ escavador/v2/resources/processo.py
@@ -15,16 +15,16 @@ class Processo(Endpoint):
     """
     methods = Method(api_version=2)
     numero_cnj: str
-    titulo_polo_ativo: Optional[str]
-    titulo_polo_passivo: Optional[str]
-    ano_inicio: Optional[int]
-    data_inicio: Optional[str]
     data_ultima_movimentacao: str  # TODO: acho que nao é optional, mas verificar
     quantidade_movimentacoes: int
     fontes_tribunais_estao_arquivadas: bool
     data_ultima_verificacao: str
     tempo_desde_ultima_verificacao: str
-    last_valid_cursor: str  # link do cursor caso queira pegar mais resultados
+    titulo_polo_ativo: Optional[str] = field(default=None)
+    titulo_polo_passivo: Optional[str] = field(default=None)
+    ano_inicio: Optional[int] = field(default=None)
+    data_inicio: Optional[str] = field(default=None)
+    last_valid_cursor: str = field(default="")  # link do cursor caso queira pegar mais resultados
     fontes: List["FonteProcesso"] = field(default_factory=list)
 
     @staticmethod
@@ -83,10 +83,10 @@ class Processo(Endpoint):
         foi bem sucedida, e False caso contrário.
 
         >>> Processo.por_nome("Escavador Engenharia e Construcoes Ltda",
-        ...                        ordena_por=CriterioOrdenacao.INICIO,
-        ...                        ordem=Ordem.DESC,
-        ...                        tribunais=[SiglaTribunal.CNJ, SiglaTribunal.TRT10],
-        ...                        qtd=1) # doctest: +SKIP
+        ...                   ordena_por=CriterioOrdenacao.INICIO,
+        ...                   ordem=Ordem.DESC,
+        ...                   tribunais=[SiglaTribunal.CNJ, SiglaTribunal.TRT10],
+        ...                   qtd=1) # doctest: +SKIP
 
         >>> Processo.por_nome("Escavador Engenharia e Construcoes Ltda") # doctest: +SKIP
         """
@@ -120,10 +120,10 @@ class Processo(Endpoint):
         foi bem sucedida, e False caso contrário.
 
         >>> Processo.por_cpf("12345678999",
-        ...                       ordena_por=CriterioOrdenacao.ULTIMA_MOVIMENTACAO,
-        ...                       ordem=Ordem.ASC,
-        ...                       tribunais=[SiglaTribunal.STF],
-        ...                       qtd=200) # doctest: +SKIP
+        ...                  ordena_por=CriterioOrdenacao.ULTIMA_MOVIMENTACAO,
+        ...                  ordem=Ordem.ASC,
+        ...                  tribunais=[SiglaTribunal.STF],
+        ...                  qtd=200) # doctest: +SKIP
 
         >>> Processo.por_cpf("123.456.789-99") # doctest: +SKIP
         """
@@ -322,16 +322,16 @@ class FonteProcesso:
     uf: str
     data_inicio: str
     data_ultima_movimentacao: str
-    segredo_justica: Optional[bool]
-    arquivado: Optional[bool]
     fisico: bool
     sistema: str
-    capa: "CapaProcessoTribunal" = field(hash=False, compare=False)
-    url: Optional[str]
-    tribunal: Optional["Tribunal"]
+    capa: "CapaProcessoTribunal" = field(hash=False, compare=False) # é omitido caso nao seja tribunal atualmente, acho que devia vir null
     quantidade_movimentacoes: int
-    caderno: Optional[str]
-    data_ultima_verificacao: Optional[str]
+    segredo_justica: Optional[bool] = field(default=None)
+    arquivado: Optional[bool] = field(default=None)
+    tribunal: Optional["Tribunal"] = field(default=None) # é omitido caso nao seja tribunal atualmente, acho que devia vir null
+    url: Optional[str] = field(default=None)
+    caderno: Optional[str] = field(default=None) # é omitido caso nao seja diario atualmente, acho que devia vir null
+    data_ultima_verificacao: Optional[str] = field(default=None)
     envolvidos: List["Envolvido"] = field(default_factory=list, hash=False, compare=False)
 
 
@@ -341,11 +341,12 @@ class CapaProcessoTribunal:
     assunto: str  # TODO: if None, get assunto_principal_normalizado.nome.upper()
     assuntos_normalizados: List["Assunto"]
     assunto_principal_normalizado: "Assunto"
-    area: Optional[str]
-    orgao_julgador: Optional[str]  # TODO: manter optional ou usar empty string?
-    valor_causa: Optional["ValorCausa"] # Sempre vem na API, mas caso o conteudo seja null, seto como None aqui
-    data_distribuicao: Optional[str]
-    data_arquivamento: Optional[str]
+    area: Optional[str] = field(default=None)
+    orgao_julgador: Optional[str] = field(default=None)  # TODO: manter optional ou usar empty string?
+    data_distribuicao: Optional[str] = field(default=None)
+    data_arquivamento: Optional[str] = field(default=None)
+    valor_causa: Optional["ValorCausa"] = field(default=None)
+    # Sempre vem na API, mas caso o conteudo seja null, seto como None aqui
     informacoes_complementares: List["InformacaoComplementar"] = field(default_factory=list)
 
 
@@ -367,7 +368,7 @@ class ValorCausa:
 class InformacaoComplementar:
     id: Optional[int]
     dado: Optional[str]
-    processo: Optional["Processo"]
+    processo: Optional[int]
     created_at: Optional[str]
     updated_at: Optional[str]
 
@@ -382,8 +383,8 @@ class InformacaoComplementar:
 class Movimentacao:
     id: int
     data: str
-    tipo: Optional[str]  # TODO: confirmar se realmente pode ser omitido/none
-    conteudo: Optional[str]  # TODO: confirmar [segredo de justiça]
+    tipo: Optional[str]  # TODO: confirmar se é sempre válido ou é omitido/null
+    conteudo: Optional[str]  # TODO: confirmar se é sempre válido [ex: caso segredo de justiça]
     fonte: "FonteMovimentacao"
 
 
@@ -395,8 +396,10 @@ class FonteMovimentacao:
     grau: int
     grau_formatado: str
     tipo: str
-    caderno: Optional[str]
+    caderno: Optional[str] # é omitido caso nao seja diario atualmente, acho que devia vir null
     tribunal: Optional["Tribunal"]
+    # Tribunal atualmente não vem na resposta da API
+    # Sugestão: fazer a API retornar, sendo o mesmo objeto Tribunal que vem em FonteProcesso
 
 
 @dataclass
@@ -404,5 +407,5 @@ class Tribunal:
     id: int
     nome: str
     sigla: str
-    categoria: str
-    estados: List[str] = field(default_factory=list, hash=False, compare=False)
+    categoria: Optional[str] = field(default=None)
+    estados: List[str] = field(default_factory=list, hash=False, compare=False) # Falta adicionar à API
