83194aab487fe6915c92b82713922ade6db50550
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2023-03-27 15:43:07 -0300

    Cria Exception para request mal-sucedido

--- escavador/exceptions.py
+++ escavador/exceptions.py
@@ -1,2 +1,30 @@
+from typing import List, Dict, Optional
+
+
 class ApiKeyNotFoundException(KeyError):
     pass
+
+
+class FailedRequest(Exception):
+    """Exceção lançada quando a API retorna um erro.
+
+    :attr status_code: código de status da resposta
+    :attr mensagem: mensagem de erro
+    :attr erros: detalhamento dos erros encontrados
+    """
+    status: int
+    code: str
+    message: str
+    errors: Dict
+
+    def __init__(self, status: int, code: str = "", message: str = "", errors: Optional[Dict] = None):
+        self.status = status
+        self.code = code
+        self.message = message
+        self.errors = errors
+
+    def __str__(self):
+        return f"Erro {self.code} ({self.status}): {self.message}"
+
+    def __repr__(self):
+        return self.__str__()
