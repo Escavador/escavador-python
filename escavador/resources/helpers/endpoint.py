8cd9a8e3d23c4af99bd24401085324bc43d72bb0
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2023-03-30 19:31:19 -0300

    Cria superclasse DataEndpoint

--- escavador/resources/helpers/endpoint.py
+++ escavador/resources/helpers/endpoint.py
@@ -1,4 +1,5 @@
-from typing import Optional
+import abc
+from typing import Optional, Dict
 
 from escavador.method import Method
 
@@ -8,3 +9,19 @@ class Endpoint(object):
     def __init__(self, api_version: Optional[int] = None):
         if api_version:
             self.methods = Method(api_version)
+
+
+class DataEndpoint(Endpoint):
+    """Um endpoint que não precisa ser instanciado e também representa um objeto retornado pela API
+
+    Exclusivamente usado na API V2.
+    """
+    __metaclass__ = abc.ABCMeta
+    methods = Method(api_version=2)
+    last_valid_cursor: Optional[str] = ""  # se não definido pela classe filha, é sempre uma string vazia
+
+    @classmethod
+    @abc.abstractmethod
+    def from_json(cls, json_dict: Optional[Dict], ultimo_cursor: str) -> Optional["DataEndpoint"]:
+        """Constrói um objeto a partir de um dicionário (json)"""
+        pass
