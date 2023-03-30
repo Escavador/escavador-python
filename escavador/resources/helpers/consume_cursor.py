cc80c45dd058af42af7b1ab196e5112e4c2b2090
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2023-03-30 19:33:08 -0300

    Substitui get_up_to por json_to_class

--- escavador/resources/helpers/consume_cursor.py
+++ escavador/resources/helpers/consume_cursor.py
@@ -1,53 +1,34 @@
 """Oferece métodos para consumir um cursor e obter os próximos resultados de uma requisição na API V2"""
 import re
-from typing import Dict, Callable, Any
+from typing import Dict, Callable, List
 
 from escavador.method import Method
 
 _methods = Method(api_version=2)
 
 
-def _consumir_cursor(cursor: str) -> Dict:
+def consumir_cursor(cursor: str) -> Dict:
     """Consome um cursor para obter os próximos resultados de uma busca
+
     :param cursor: url do cursor a ser consumido
-    :return: um dicionário com a resposta da requisição
+    :return: a resposta da requisição
     """
     endpoint_cursor = re.sub(r".*/api/v\d/", "", cursor)
     return _methods.get(endpoint_cursor)
 
 
-def get_up_to(resposta: Dict, qtd: int, constructor: Callable) -> Any:
-    """Obtém os próximos resultados de uma busca até atingir a quantidade desejada ou erro
+def json_to_class(resposta: Dict, constructor: Callable, add_cursor=False) -> List:
+    """Instancia os itens de uma resposta a partir de um construtor
 
-    :param resposta: a resposta da primeira requisição
-    :param qtd: a quantidade de resultados desejada
+    :param resposta: a resposta da primeira requisição, onde 'items' é uma lista de dicts (jsons)
     :param constructor: método para construir um objeto da resposta a partir do json
-    :return: uma resposta extendida com até `qtd` resultados
+    :param add_cursor: se True, adiciona o cursor da resposta ao objeto instanciado
+    :return: uma lista de objetos instanciados
     """
-    while 0 < len(resposta["resposta"].get("items", [])) < qtd:
-        cursor = resposta["resposta"].get("links", {}).get("next")
-        if not cursor:
-            break
-
-        next_response = _consumir_cursor(cursor)
-        next_items = next_response["resposta"].get("items")
-        if not next_items:
-            resposta["http_status"] = next_response["http_status"]
-            break
-
-        resposta["resposta"]["items"].extend(next_items)
-
-        # replace cursor with next cursor
-        resposta["resposta"]["links"]["next"] = (
-            next_response["resposta"].get("links", {}).get("next")
-        )
-
-    if "items" in resposta["resposta"]:
-        items = resposta["resposta"]["items"][:qtd]
-        cursor = resposta["resposta"].get("links", {}).get("next", "")
-        objetos = [constructor(item) for item in items]
-        for objeto in objetos:
-            objeto.last_valid_cursor = cursor
-        return objetos
-
-    return resposta
+    items = resposta["resposta"]["items"]
+    cursor_url = resposta["resposta"].get("links", {}).get("next", "")
+    return (
+        [constructor(item, ultimo_cursor=cursor_url) for item in items]
+        if add_cursor
+        else [constructor(item) for item in items]
+    )
