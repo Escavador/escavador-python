9af71884f20ea5083f32a0211f8ad49567d01c81
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2023-06-12 09:22:22 -0300

    Cria classe ListaResultados

---
+++ escavador/resources/helpers/lista_resultados.py
@@ -0,0 +1,34 @@
+class ListaResultados(list):
+
+    def continuar_busca(self) -> "ListaResultados":
+        """Retorna a próxima página de resultados, caso exista."""
+        return self[-1].continuar_busca() if len(self) else ListaResultados()
+
+    def mais_paginas(self, num_paginas: int = 1) -> int:
+        """Extende a lista de resultados com mais resultados, caso existam.
+
+        :param num_paginas: informa quantas páginas adicionais devem ser solicitadas.
+        Se omitido, busca-se apenas a próxima página.
+        :return: Número de páginas adicionais recebidas com sucesso.
+        """
+        return sum(int(self._mais_uma_pagina()) for _ in range(num_paginas))
+
+    def _mais_uma_pagina(self) -> bool:
+        """Continua a busca de resultados, caso existam mais páginas.
+
+        :return: True se recebeu uma nova página com sucesso, False caso contrário.
+        """
+        if not len(self):
+            return False
+
+        novos_resultados = self[-1].continuar_busca()
+
+        if not novos_resultados:
+            return False
+
+        if len(novos_resultados) > 1 and isinstance(novos_resultados[1], ListaResultados):
+            novos_resultados = novos_resultados[1]
+
+        self.extend(novos_resultados)
+
+        return True
