2ad7762fea237960662d51944958d2a024cdd037
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2023-08-09 08:46:06 -0300

    Melhora test_documento.py

--- tests/resources/helpers/test_documento.py
+++ tests/resources/helpers/test_documento.py
@@ -6,21 +6,28 @@ from tempfile import NamedTemporaryFile
 
 class TestDocumento(unittest.TestCase):
     def test_get_pdf(self):
-        content = b"".join([bytes(chr(randint(0, 50000)), "utf-8") for i in range(10000)])
+        content = b"".join([bytes(chr(randint(0, 50000)), "utf-8") for i in range(100000)])
         with NamedTemporaryFile("wb") as temp:
             name = temp.name.split("\\")[-1]
             path = "\\".join(temp.name.split("\\")[:-1])
-            Documento.get_pdf(
+            realpath = Documento.get_pdf(
                 content,
                 path,
                 name
-            )
-            # read from the tempfile
-            with open(temp.name+'.pdf', "rb") as temp2:
+            )["path"]
+            self.assertEqual(str(realpath), temp.name+'.pdf')
+            with open(realpath, "rb") as temp2:
                 self.assertEqual(
                     temp2.read(),
                     content
                 )
+
+            erro = Documento.get_pdf(
+                content,
+                path,
+                name
+            )["error"]
+            self.assertEqual(erro, "File exists")
 
 
 if __name__ == "__main__":
